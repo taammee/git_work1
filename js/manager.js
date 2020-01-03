23e08572b2391273eb3a62a0addfb0cddc479101
taammee <moworkon@gmail.com>
2020-01-03 10:25:35 +0900

    관리자페이지 수정

--- js/manager.js
+++ js/manager.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
         $('#nav > li').removeClass('_on');
         $(this).addClass('_on');
     });
+    // 기존 키워드 삭제 & 기존 키워드가 없을때 dd 삭제
+    $('._keyAdd > dd > span > ._del').click(function(){
+        $(this).parent('span').remove();
+        $('._keyAdd > dd').each(function(){
+            var keyL = $(this).find('> span').length;
+            if (keyL < 1) {
+                $(this).remove();
+            }
+        }); 
+    });
+
 });
